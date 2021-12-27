d925b4914ac6de2162cf11e2847e81a255a6d5bb
Sagar Shrivastava <sagar.shrivastava6413@gmail.com>
2021-12-27 13:16:16 +0530

    Updated 7Up 7 down game

--- 7 up 7 down/script.js
+++ 7 up 7 down/script.js
@@ -7,11 +7,47 @@ const secondDiceImage = "dice" + secondRandom + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
 document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
 
-//Logic winner
-if (firstRandom > secondRandom) {
-  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
-} else if (firstRandom < secondRandom) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
-} else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+let choice = 0;
+
+up.addEventListener("click", () => {
+  choice = 8;
+});
+down.addEventListener("click", () => {
+  choice = 6;
+});
+up.addEventListener("click", () => {
+  choice = 7;
+});
+roll.addEventListener("click", () => {
+  checkWinner();
+});
+const checkWinner = () => {
+  const firstRandom = Math.floor(Math.random() * 6) + 1;
+  const secondRandom = Math.floor(Math.random() * 6) + 1;
+
+  const firstDiceImage = "dice" + firstRandom + ".png";
+  const secondDiceImage = "dice" + secondRandom + ".png";
+
+  document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
+  document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
+
+  if (choice > 7) {
+    if (firstRandom + secondRandom > 7) {
+      document.querySelector("h1").innerHTML = "You Won!";
+    } else {
+      document.querySelector("h1").innerHTML = "You Lost";
+    }
+  } else if (choice < 7) {
+    if (firstRandom + secondRandom < 7) {
+      document.querySelector("h1").innerHTML = "You Won!";
+    } else {
+      document.querySelector("h1").innerHTML = "You Lost";
+    }
+  } else {
+    if (firstRandom + secondRandom === 7) {
+      document.querySelector("h1").innerHTML = "You Won!";
+    } else {
+      document.querySelector("h1").innerHTML = "You Lost";
+    }
+  }
+};
