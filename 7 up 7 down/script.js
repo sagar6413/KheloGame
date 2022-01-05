06ca7e144ec7d0fe833dc7246f237653a6934be7
Sagar Shrivastava <sagar.shrivastava6413@gmail.com>
2022-01-05 17:09:20 +0530

    modified 7up 7down

--- 7 up 7 down/script.js
+++ 7 up 7 down/script.js
@@ -1,8 +1,8 @@
 const firstRandom = Math.floor(Math.random() * 6) + 1;
 const secondRandom = Math.floor(Math.random() * 6) + 1;
 
-const firstDiceImage = "dice" + firstRandom + ".png";
-const secondDiceImage = "dice" + secondRandom + ".png";
+const firstDiceImage = "qmark.jpg";
+const secondDiceImage = "qmark.jpg";
 
 document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
 document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
@@ -11,16 +11,20 @@ let choice = 0;
 
 up.addEventListener("click", () => {
   choice = 8;
+  document.querySelector(".up").style.background = "#00FF00";
 });
 down.addEventListener("click", () => {
   choice = 6;
+  document.querySelector(".down").style.background = "#00FF00";
 });
-up.addEventListener("click", () => {
+lucky.addEventListener("click", () => {
   choice = 7;
+  document.querySelector(".lucky").style.background = "#00FF00";
 });
 roll.addEventListener("click", () => {
   checkWinner();
 });
+
 const checkWinner = () => {
   const firstRandom = Math.floor(Math.random() * 6) + 1;
   const secondRandom = Math.floor(Math.random() * 6) + 1;
@@ -50,4 +54,14 @@ const checkWinner = () => {
       document.querySelector("h1").innerHTML = "You Lost";
     }
   }
+  setTimeout(() => {
+    const firstDiceImage = "qmark.jpg";
+    const secondDiceImage = "qmark.jpg";
+    document.querySelector("h1").innerHTML = "Play Again";
+    document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
+    document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
+    document.querySelectorAll(".play")[0].style.background = "#f7b7f7";
+    document.querySelectorAll(".play")[1].style.background = "#f7b7f7";
+    document.querySelectorAll(".play")[2].style.background = "#f7b7f7";
+  }, 2500);
 };
